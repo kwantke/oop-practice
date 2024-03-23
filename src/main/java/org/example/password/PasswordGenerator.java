1fae87730cf97019caaffd4010a94b5db4213232
kokk <kokk7310@gmail.com>
2024-03-23 20:02:42 +0900

    비밃번호 검증 테스트 요구사항 • 비밀번호는 최소 8자 이상 12자 이하여야 한다. • 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생시킨다. • 경계조건에 대해 테스트 코드를 작성해야 한다.

---
+++ src/main/java/org/example/password/PasswordGenerator.java
@@ -0,0 +1,8 @@
+package org.example.password;
+
+//@FunctionalInterface 함수형 인터페이스란 https://zzang9ha.tistory.com/303
+@FunctionalInterface
+public interface PasswordGenerator{
+
+  String generatePassword();
+}
